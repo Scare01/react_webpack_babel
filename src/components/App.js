cc21e9192325a4dc43fb8850676686c1f511fbd4
Aleksei Slesarev <aleksei@Mac-mini-Aleksei.local>
2019-04-23 11:57:57 +0700

    add eslint

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,10 @@ import React from 'react';
 
 import '../styles/App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello</h1>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div>
+    <h1>Hello</h1>
+  </div>
+);
 
 export default App;
